2897e4b58428aaa12ec25dc7dfc5f5f2181dfa07
suchithrasethi8658cls <suchitrasethi8658@gmail.com>
2022-06-22 18:41:14 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def suchitra(request):
+    return render(request,'suchitra.html')
---
+++ app3/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def suchi(request):
+    return render(request,'suchi.html')
